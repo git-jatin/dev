a62120b9e9de1b0c175647f01ae9c9791b9de535
git-jatin <jatinrai@Jatins-MacBook-Air.local>
2025-03-27 13:03:09 +0530

    worked upon

---
+++ string reverse.py
@@ -0,0 +1,8 @@
+a=input("please enter the string")
+
+print(''.join(reversed(a))) ##reverse string letter wise
+#w=a.split(".")
+#print(w)
+#print(".".join(reversed(w)))
+
+#split  =string into list
